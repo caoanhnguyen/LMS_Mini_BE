4df3d924c8c665cd07448eaa3c13d3e7a0b79bda
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-11-17 15:19:00 +0700

    feat(config): setup global exception handler and i18n

---
+++ src/main/java/com/example/lms_mini/dto/DataResponse.java
@@ -0,0 +1,16 @@
+package com.example.lms_mini.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class DataResponse<T> {
+    private final int status;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+}
