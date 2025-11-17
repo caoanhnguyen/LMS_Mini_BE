b9121e9cc68bd093da0a8cc251b28b8384725809
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-11-17 09:20:28 +0700

    Initial Commit

---
+++ src/main/java/com/example/lms_mini/LmsMiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.lms_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsMiniApplication.class, args);
+    }
+
+}
